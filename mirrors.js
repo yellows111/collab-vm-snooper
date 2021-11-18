2b987f460476e43fa08a0c084b7a28490b41dbd9
yellows111 <ice_ice@email.com>
2021-11-18 23:28:55 +0000

    ignore mirrors

--- mirrors.js
+++ mirrors.js
@@ -1,4 +1,4 @@
-module.exports =  {
+module.exports = {
 	mainvms:[], // put main vms here, use a ! prefix to denote WSS
 	uservms:[], // put current user-vm additionalNodes here
 	externalvms:[] // put any third party collab-vm instances (not on main or user) here
